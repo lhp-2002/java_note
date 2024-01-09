3de52234cbdeb26ce8d25eb65f61f8133c3bf4d9
lhp-2002 <3219845990@qq.com>
2024-01-09 14:07:01 +0800

    test

--- Test.java
+++ Test.java
@@ -1,5 +1,34 @@
 public class Test{
     public static void main(String []args) {
-        System.out.println("Hello,world");
+        // 输出Hello，world
+        // System.out.println("Hello,world,main");
+        // System.out.println("name" + "张三");
+        // System.out.println("age" + "28");
+        // 制表符  美化输出
+        // System.out.println("name" + '\t' + "张三");
+        // System.out.println("age" + '\t' + "28");
+
+        // 变量
+        // int a = 10, b = 15, c = 30;
+        // System.out.println(a);
+        // System.out.println(b);
+
+        // a = 20;
+        // System.out.println(a);
+        // System.out.println(c);
+        int num = 0;
+        int [] a = {1,2,2,0,1};
+        int [] b = {0,1,1,1,0};
+        for (int i=0; i<a.length; i++){
+            if (a[i] >= 1){
+                num = num + a[i];
+            }
+
+            if (b[i] >= 1){
+                num = num - b[i];
+            }
+        }
+
+        System.out.println(num);
     }
-}
+ }

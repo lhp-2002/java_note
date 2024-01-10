f32bd7125109f5ec1c4d3bf2f500427ab0a0068c
lhp-2002 <3219845990@qq.com>
2024-01-10 16:59:55 +0800

    2024/1/10  更新

--- Test.java
+++ Test.java
@@ -16,19 +16,30 @@ public class Test{
         // a = 20;
         // System.out.println(a);
         // System.out.println(c);
-        int num = 0;
-        int [] a = {1,2,2,0,1};
-        int [] b = {0,1,1,1,0};
-        for (int i=0; i<a.length; i++){
-            if (a[i] >= 1){
-                num = num + a[i];
-            }
+        // int num = 0;
+        // int [] a = {1,2,2,0,1};
+        // int [] b = {0,1,1,1,0};
+        // for (int i=0; i<a.length; i++){
+        //     if (a[i] >= 1){
+        //         num = num + a[i];
+        //     }
 
-            if (b[i] >= 1){
-                num = num - b[i];
-            }
-        }
+        //     if (b[i] >= 1){
+        //         num = num - b[i];
+        //     }
+        // }
 
-        System.out.println(num);
+        // System.out.println(num);
+        byte a = -101;   // 不能超过127，负数不能超过-128
+        System.out.println(a);
+
+        short b = 30000;
+        System.out.println(b);
+
+        long c = 9999999999L;  //long类型大于9位数时，需要在后面加个L
+        System.out.println(c);
+
+        float d = 198.89F; //float类型，定义数据值时需要在后面加个F
+        System.out.println(d);
     }
  }

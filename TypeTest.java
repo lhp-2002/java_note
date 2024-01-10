ea5caa517a92d7b108fda512e12983854109a479
lhp-2002 <3219845990@qq.com>
2024-01-10 17:33:37 +0800

    新文件上传

---
+++ TypeTest.java
@@ -0,0 +1,16 @@
+public class TypeTest {
+    public static void main(String[] args){
+        // 数据类型
+        String name = "张三";   //字符串类型
+        int age = 18;       //整数类型
+        char sex = '男';    //字符类型
+        double height = 182.6;  //小数点类型，浮点数类型
+        boolean single = true;  //布尔类型
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(sex);
+        System.out.println(height);
+        System.out.println(single); 
+    }
+}

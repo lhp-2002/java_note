aadc902cc286d67f0f338b68a59a3a81d2e448ae
lhp-2002 <3219845990@qq.com>
2024-01-15 10:14:54 +0800

    2024/1/15  更新版本

--- TypeTest.java
+++ TypeTest.java
@@ -1,3 +1,4 @@
+import java.util.Date;
 public class TypeTest {
     public static void main(String[] args){
         // 数据类型
@@ -6,11 +7,18 @@ public class TypeTest {
         char sex = '男';    //字符类型
         double height = 182.6;  //小数点类型，浮点数类型
         boolean single = true;  //布尔类型
+        // 获取当前时间戳
+        long timestamp = System.currentTimeMillis();
+        Date dateObject = new Date(timestamp);
+        long newTimestamp = dateObject.getTime();
+
+        System.out.println(timestamp);
+        System.out.println(newTimestamp);
 
         System.out.println(name);
         System.out.println(age);
         System.out.println(sex);
         System.out.println(height);
-        System.out.println(single); 
+        System.out.println(single);  
     }
 }
